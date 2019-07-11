6b1aa98954aa88d14355349a63368dba1a765398
luncheon <takeuck+github@gmail.com>
2019-07-11 21:07:15 +0900

    ファイル名変更

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -5515,7 +5515,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55233" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58254" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
