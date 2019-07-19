8324577018a20b077504e06683e16d9dac784f88
luncheon <takeuck+github@gmail.com>
2019-07-19 19:57:08 +0900

    いろいろ調整

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 
 import Reveal from 'reveal.js'
 import 'reveal.js/css/reveal.css'
-import 'reveal.js/css/theme/black.css'
+import './theme.scss'
 
 import 'highlight.js/styles/github.css'
 import 'diff2html/dist/diff2html.min.css'
 
+import './typeface.css'
 import './styles.css'
 
 Reveal.initialize({ hash: true })
