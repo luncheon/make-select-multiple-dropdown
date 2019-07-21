79713d58986c65b5ffc39bd0402f9d372e577604
luncheon <takeuck+github@gmail.com>
2019-07-22 03:00:57 +0900

    いろいろ調整

--- src/index.js
+++ src/index.js
@@ -9,4 +9,21 @@ import 'diff2html/dist/diff2html.min.css'
 import './typeface.css'
 import './styles.css'
 
+function onSlideShown(event) {
+  var code = event.currentSlide.getElementsByClassName('code')[0]
+  var firstChangedCell = code && code.querySelector('td.d2h-del,td.d2h-ins')
+  var firstChangedRow = firstChangedCell && firstChangedCell.parentElement
+  if (!firstChangedRow || firstChangedRow.rowIndex < 12) {
+    return
+  }
+  for (
+    var row = firstChangedRow.previousElementSibling;
+    row && !row.querySelector('.hljs-selector-attr,.hljs-tag');
+    row = row.previousElementSibling
+  );
+  row && row.previousElementSibling && row.previousElementSibling.scrollIntoView()
+}
+
+Reveal.addEventListener('ready', onSlideShown)
+Reveal.addEventListener('slidechanged', onSlideShown)
 Reveal.initialize({ hash: true })
